014256338076b1517953bf5544b5c15f4a9f0f04
polsky777 <t.polsky@gmail.com>
2020-06-02 19:39:16 +0200

    1

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    { 
+            static void Main(string[] args)
+            {
+                DateTimeProvider dateTimeProvider = new DateTimeProvider();
+                AgeCalculator ageCalculator = new AgeCalculator(dateTimeProvider);
+                DateTime BirthDay = new DateTime(2005, 05, 10);
+                int Age = ageCalculator.GetAge(BirthDay);
+                Console.WriteLine(Age);
+                Console.ReadKey();
+            }
+
+            public class AgeCalculator
+        {
+            private readonly IDateTimeProvider _dateTimeProvider;
+
+            public AgeCalculator(IDateTimeProvider dateTimeProvider)
+            {
+                if (dateTimeProvider == null) throw new ArgumentNullException(nameof(dateTimeProvider));
+                _dateTimeProvider = dateTimeProvider;
+            }
+
+            public int GetAge(DateTime dateOfBirth)
+            {
+                DateTime now = DateTime.Now;
+                int age = now.Year - dateOfBirth.Year;
+
+                if (now.Month < dateOfBirth.Month ||
+                   (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
+                {
+                    age--;
+                }
+
+                return age;
+            }
+        }
+
+        public interface IDateTimeProvider
+        {
+            DateTime GetDateTime();
+        }
+        public class DateTimeProvider : IDateTimeProvider
+        {
+            public DateTime GetDateTime() => DateTime.Now;
+        }
+
+    }
+}
+    
---
+++ ConsoleApp4/Test.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using static ConsoleApp4.Program;
+
+namespace ConsoleApp4
+{
+    [TestFixture]
+    public class Testy
+    {
+        [Test]
+        public void AttackArmourBreakTest()
+        {
+            // Arrange:
+            var currentDate = new DateTime(2015, 1, 1);
+            var dateOfBirth = new DateTime(1990, 1, 1);
+            var dateTimeProvider = Mock.Of<IDateTimeProvider>(provider =>
+              provider.GetDateTime() == currentDate);
+            var ageCalculator = new AgeCalculator(dateTimeProvider);
+
+            // Act:
+            int age = ageCalculator.GetAge(dateOfBirth);
+
+            // Assert:
+            Assert.AreEqual(25, age);
+
+        }
+    }
+}
